6a67af8421a52ab11830e632d99feb08b9ec8ad9
suhail544 <suhailahamed240@gmail.com>
2025-09-21 15:16:33 +0530

    fix

--- project-quiz-backend/app.js
+++ project-quiz-backend/app.js
@@ -6,12 +6,10 @@ const app = express()
 const quizRoute = require('./router/quizRouter')
 const scoreRoute = require('./router/scoreRouter')
 
-const cors = require('cors');
-app.use(cors()); // allow all origins for development
 
 
 app.use(express.json())
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 app.use('/api/quiz/question', quizRoute)
 app.use('/api/quiz/score', scoreRoute)
 
